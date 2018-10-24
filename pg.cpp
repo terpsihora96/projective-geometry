fb60ba7b0ba3cae90fd4ddd69b41452c549bed4e
terpsihora96 <dzedaj996@gmail.com>
2018-10-24 12:29:33 +0200

    Naive algorithm implemented...

--- pg.cpp
+++ pg.cpp
@@ -4,13 +4,12 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
 using namespace cv;
 
 int i = 0;
 int j = 0;
-Eigen::MatrixXf M(4,3);
-Eigen::MatrixXf M1(4,3);
+Eigen::MatrixXf M(3, 4);
+Eigen::MatrixXf M1(3, 4);
 
 void callbackFuncImg (int event, int x, int y, int, void*)
 {
@@ -18,9 +17,9 @@ void callbackFuncImg (int event, int x, int y, int, void*)
         return;
     } else if (event ==  EVENT_LBUTTONDOWN) {
         std::cout << "(" << y << "," << x << ")" << std::endl;
-        M(i, 0) = y;
-        M(i, 1) = x;
-        M(i, 2) = 1;
+        M(0, i) = y;
+        M(1, i) = x;
+        M(2, i) = 1;
         i += 1;
     }
 }
@@ -31,9 +30,9 @@ void callbackFuncImg1 (int event, int x, int y, int, void*)
         return;
     } else if (event ==  EVENT_LBUTTONDOWN) {
         std::cout << "(" << y << "," << x << ")" << std::endl;
-        M1(j, 0) = y;
-        M1(j, 1) = x;
-        M1(j, 2) = 1;
+        M1(0, j) = y;
+        M1(1, j) = x;
+        M1(2, j) = 1;
         j += 1;
     }
 }
@@ -41,56 +40,85 @@ void callbackFuncImg1 (int event, int x, int y, int, void*)
 int main (int argc, char** argv)
 {
     if (argc != 2) {
-        std::cout << "Usage: ./pg image_name" << std::endl;
+        std::cerr << "Usage: ./pg image_name" << std::endl;
         return -1;
     }
 
     Mat image;
-    image =  imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    unsigned width = image.cols;
-    unsigned height = image.rows;    
-
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     if (image.empty()) {
-        std::cout <<  "Could not open or find the image" << std::endl ;
+        std::cerr << "Failed to open an image." << std::endl;
         return -1;
     }
+
+    unsigned height = image.rows;
+    unsigned width = image.cols;
+    Mat new_image (height, width, CV_8UC3);
 
     namedWindow(argv[1],  WINDOW_AUTOSIZE);
     setMouseCallback(argv[1], callbackFuncImg, NULL);
     imshow(argv[1], image);
     waitKey(0);
 
-    Mat new_image =  Mat::ones(height, width, CV_8UC3);
-    namedWindow("New image",  WINDOW_AUTOSIZE);
-    setMouseCallback("New image", callbackFuncImg1, NULL);
-    imshow("New image", new_image);
+    setMouseCallback(argv[1], callbackFuncImg1, NULL);
+    imshow(argv[1], image);
     waitKey(0);
 
-    M << 1,0,0,0,1,0,0,0,1,1,1,1;
-    M1 << 1,2,3,3,2,1,0,1,1,7,11,10;
+    //M << -3,3,1,-1,-1,-1,1,1,1,1,1,1;
+    //M1 << -2,2,2,-2,-1,-1,1,1,1,1,1,1;
+    M << 1,0,0,1,0,1,0,1,0,0,1,1;
+    M1 << 1,3,0,7,2,2,1,11,3,1,1,10;
     std::cout << "Matrix M\n\n" << M << std::endl;
     std::cout << "Matrix M1\n\n" << M1 << std::endl;
     
-    Eigen::Vector3f D = M.block<1,3>(3,0).transpose();
-    Eigen::Vector3f D1 = M1.block<1,3>(3,0).transpose();
-
-    Eigen::Vector3f lambda = (M.block<3,3>(0,0)).inverse() * (D);
-    Eigen::Vector3f lambda1 = (M1.block<3,3>(0,0)).inverse() * (D1);
+    Eigen::Vector3f D = M.rightCols<1>();
+    Eigen::Vector3f D1 = M1.rightCols<1>();
+    std::cout << D << std::endl << std::endl;
+    std::cout << D1 << std::endl << std::endl;
+    Eigen::Vector3f lambda = (M.leftCols<3>().inverse()) * D;
+    Eigen::Vector3f lambda1 = (M1.leftCols<3>().inverse()) * D1;
     
-    cout << "\nSolution\n";
-    cout << "1:\n" << lambda << endl;
-    cout << "2:\n" << lambda1 << endl;
+    std::cout << "lambda1:\n" << lambda << std::endl;
+    std::cout << "lambda2:\n" << lambda1 << std::endl;
 
     Eigen::MatrixXf P1(3,3);
-    P1 = (M.block<3,3>(0,0)).array().colwise() * lambda.array();
     Eigen::MatrixXf P2(3,3);
-    P2 = (M1.block<3,3>(0,0)).array().colwise() * lambda1.array();
+    P1 = (M.leftCols<3>()).array().rowwise() * lambda.transpose().array();
+    P2 = (M1.leftCols<3>()).array().rowwise() * lambda1.transpose().array();
+    std::cout << P1 << std::endl;
+    std::cout << P2 << std::endl;
     
     Eigen::MatrixXf P(3,3);
     Eigen::MatrixXf P_inverse(3,3);
     P = P2 * P1.inverse();
+    std::cout << "P\n" << std::endl << P << std::endl;
     P_inverse = P.inverse();
-    cout << P << endl;
+    std::cout << "P_inverse\n" << std::endl << P_inverse << std::endl;
+    
+    for (unsigned i = 0; i <= height; ++i) {
+        for (unsigned j = 0; j <= width; ++j) {
+            Eigen::Vector3f m1_pixel;
+            m1_pixel << i, j, 1;
+            Eigen::Vector3f m_pixel = P_inverse * m1_pixel;
+            
+            m_pixel /= m_pixel[2];
+            
+            int x = std::round(m_pixel[0]);
+            int y = std::round(m_pixel[1]);
+            std::cout << m_pixel[0] << " " << x << std::endl;
+            std::cout << m_pixel[1] << " " << y << std::endl;
+            if (x >= 0 and y >= 0 and x < (int)width and y < (int)height) {
+                Vec3b color = image.at<Vec3b>(Point(x, y));
+                new_image.at<Vec3b>(Point(i, j)) = color;
+            }
+        }
+    }
+    
+    namedWindow("New image",  WINDOW_AUTOSIZE);
+    imshow("New image", new_image);
+    waitKey(0);
+
+    imwrite("./new_image.png", new_image);
     
     return 0;
 }
