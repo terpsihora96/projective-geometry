3dda67b47de722a6e86c319c572abe5679ceb5fe
terpsihora96 <dzedaj996@gmail.com>
2018-10-26 11:07:33 +0200

    Code polished.

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
-CXX     = g++
-CXXFLAGS = -std=c++17 -I include
-LDLIBS  = -lstdc++fs `pkg-config opencv --cflags --libs`
-WFLAGS = -Wall -Wextra -O3
+CXX     	= g++
+CXXFLAGS 	= -std=c++17 -I include
+LDLIBS  	= -lstdc++fs `pkg-config opencv --cflags --libs`
+WFLAGS 		= -Wall -Wextra -O3
 
-pg: pg.cpp
+fix_distortion: fix_distortion.cpp
 	$(CXX) -o $@ $^ $(CXXFLAGS) $(WFLAGS) $(LDLIBS) 
-
--- fix_distortion.cpp
+++ fix_distortion.cpp
@@ -6,33 +6,31 @@
 
 using namespace cv;
 
-int i = 0;
-int j = 0;
 Eigen::MatrixXf M(3, 4);
 Eigen::MatrixXf M1(3, 4);
 
 void callbackFuncImg (int event, int x, int y, int, void*)
 {
+    static int i = 0;
+
     if (i > 3) {
         return;
     } else if (event ==  EVENT_LBUTTONDOWN) {
         std::cout << "(" << y << "," << x << ")" << std::endl;
-        M(0, i) = y;
-        M(1, i) = x;
-        M(2, i) = 1;
+        M.col(i) << y, x, 1;
         i += 1;
     }
 }
 
 void callbackFuncImg1 (int event, int x, int y, int, void*)
 {
+    static int j = 0;
+
     if (j > 3) {
         return;
     } else if (event ==  EVENT_LBUTTONDOWN) {
         std::cout << "(" << y << "," << x << ")" << std::endl;
-        M1(0, j) = y;
-        M1(1, j) = x;
-        M1(2, j) = 1;
+        M1.col(j) << y, x, 1;
         j += 1;
     }
 }
@@ -53,9 +51,9 @@ int main (int argc, char** argv)
 
     unsigned height = image.rows;
     unsigned width = image.cols;
-    Mat new_image (height, width, CV_8UC3);
+    Mat new_image(height, width, CV_8UC3);
 
-    namedWindow(argv[1],  WINDOW_AUTOSIZE);
+    namedWindow(argv[1],  WINDOW_NORMAL);
     setMouseCallback(argv[1], callbackFuncImg, NULL);
     imshow(argv[1], image);
     waitKey(0);
@@ -64,36 +62,24 @@ int main (int argc, char** argv)
     imshow(argv[1], image);
     waitKey(0);
 
-    //M << -3,3,1,-1,-1,-1,1,1,1,1,1,1;
-    //M1 << -2,2,2,-2,-1,-1,1,1,1,1,1,1;
-    //M << 1,0,0,1,0,1,0,1,0,0,1,1;
-    //M1 << 1,3,0,7,2,2,1,11,3,1,1,10;
-    std::cout << "Matrix M\n\n" << M << std::endl;
-    std::cout << "Matrix M1\n\n" << M1 << std::endl;
+    std::cout << "\nMatrix M\n" << M << std::endl;
+    std::cout << "\nMatrix M1\n" << M1 << std::endl;
     
     Eigen::Vector3f D = M.rightCols<1>();
     Eigen::Vector3f D1 = M1.rightCols<1>();
-    std::cout << D << std::endl << std::endl;
-    std::cout << D1 << std::endl << std::endl;
+    
     Eigen::Vector3f lambda = (M.leftCols<3>().inverse()) * D;
     Eigen::Vector3f lambda1 = (M1.leftCols<3>().inverse()) * D1;
-    
-    std::cout << "lambda1:\n" << lambda << std::endl;
-    std::cout << "lambda2:\n" << lambda1 << std::endl;
 
     Eigen::MatrixXf P1(3,3);
     Eigen::MatrixXf P2(3,3);
     P1 = (M.leftCols<3>()).array().rowwise() * lambda.transpose().array();
     P2 = (M1.leftCols<3>()).array().rowwise() * lambda1.transpose().array();
-    std::cout << P1 << std::endl;
-    std::cout << P2 << std::endl;
-    
+
     Eigen::MatrixXf P(3,3);
     Eigen::MatrixXf P_inverse(3,3);
     P = P2 * P1.inverse();
-    std::cout << "P\n" << std::endl << P << std::endl;
     P_inverse = P.inverse();
-    std::cout << "P_inverse\n" << std::endl << P_inverse << std::endl;
     
     for (unsigned i = 0; i < height; ++i) {
         for (unsigned j = 0; j < width; ++j) {
@@ -102,19 +88,17 @@ int main (int argc, char** argv)
             Eigen::Vector3f m_pixel = P_inverse * m1_pixel;
             
             m_pixel /= m_pixel[2];
-            
-            int x = std::round(m_pixel[0]);
-            int y = std::round(m_pixel[1]);
-            //std::cout << m_pixel[0] << " " << x << std::endl;
-            //std::cout << m_pixel[1] << " " << y << std::endl;
-            if (x >= 0 and y >= 0 and x < (int)height and y < (int)width) {
-                Vec3b color = image.at<Vec3b>(Point(y,x));
+            int i1 = std::round(m_pixel[0]);
+            int j1 = std::round(m_pixel[1]);
+
+            if (i1 >= 0 and i1 < (int)height and j1 >= 0 and j1 < (int)width) {
+                Vec3b color = image.at<Vec3b>(Point(j1,i1));
                 new_image.at<Vec3b>(Point(j,i)) = color;
             }
         }
     }
     
-    namedWindow("New image",  WINDOW_AUTOSIZE);
+    namedWindow("New image",  WINDOW_NORMAL);
     imshow("New image", new_image);
     waitKey(0);
 
